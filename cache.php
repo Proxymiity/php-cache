218d51e8655b9adbbbbc1db5b8da57c9dfc6ae80
Proxymiity <commits@proxymiity.fr>
2021-05-11 18:05:03 +0200

    sending data type based on what's downloaded

--- cache.php
+++ cache.php
@@ -10,15 +10,10 @@
     if (isset($_GET["refresh"])){
         rem_cache($_GET["url"]);
     }
-    if (cached($_GET["url"])){
-        header('Content-Type: ' . get_mime("./data/" . md5($_GET["url"]) . "/" . basename($_GET["url"])));
-        header('Content-Length: ' . filesize("./data/" . md5($_GET["url"]) . "/" . basename($_GET["url"])));
-        header('Accept-Range: bytes');
-        echo load_cache($_GET["url"]);
-    } else {
+    if (!cached($_GET["url"])) {
         cache($_GET["url"], $_GET["referer"], $_GET["user"], $_GET["pass"]);
-        header('Content-Type: ' . get_mime($_GET["url"]));
-        header('Content-Length: ' . filesize("./data/" . md5($_GET["url"]) . "/" . basename($_GET["url"])));
-        header('Accept-Range: bytes');
-        echo load_cache($_GET["url"]);
     }
+    header('Content-Type: ' . get_mime("./data/" . md5($_GET["url"]) . "/" . basename($_GET["url"])));
+    header('Content-Length: ' . filesize("./data/" . md5($_GET["url"]) . "/" . basename($_GET["url"])));
+    header('Accept-Range: bytes');
+    echo load_cache($_GET["url"]);
